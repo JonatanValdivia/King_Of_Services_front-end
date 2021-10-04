4cab6f436cfb51e9a38a2d4e75e73635f2a41426
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-10-04 17:55:13 -0300

    Aplicadas as funcoes de find (na input) e index (padrão)

--- app/src/App.js
+++ app/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <GlobalStyleComponent/>
       {/* <Home/> */}
       {/* <Login /> */}
-      <Cadastro/>
+      <Router/>
       
     </>
   );
--- app/src/components/Input/index.js
+++ app/src/components/Input/index.js
@@ -1,10 +1,36 @@
 import { Container } from "./style";
 
 function Input(){
+    const getAllProviders = async() =>{
+        fetch('http://kingofservices.com.br/Prestadores')
+        .then((response) => response.json())
+        .then((responseJson) => (
+            console.log(responseJson)
+            //setData(responseJson.records)
+        ));
+      };
+      //getCLient();
+
+      const getAllProvidersOfSearch = async(profession) =>{
+        fetch(`http://kingofservices.com.br/PesquisarPrestador/${profession}`)
+        .then((response) => response.json())
+        .then((responseJson) => (
+            console.log(responseJson)
+            //setData(responseJson.records)
+        ));
+      };
+
+      getAllProvidersOfSearch('pintor')
+
+      const search = async () =>{
+          const input = document.getElementById('search').value
+          getAllProvidersOfSearch(input)
+      }
+
     return(
         <Container>
-            <input placeholder="Informe o que precisa..." />
-            <button>Buscar</button>
+            <input placeholder="Informe o que precisa..." id="search"/>
+            <button onClick={search}>Buscar</button>
         </Container>
     );
 }
