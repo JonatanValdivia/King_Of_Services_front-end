21cf2113badfd673a80ae94e62f6930ac8a5fef4
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-12-07 19:21:45 -0300

    Implementacao do pagamento

--- app/src/components/CardConcluido/index.js
+++ app/src/components/CardConcluido/index.js
@@ -1,6 +1,11 @@
 import { ButtonProgress, CardProgressStyle, ContentCard, Profile } from './style';
 import { AiFillCheckCircle } from "react-icons/ai";
 function CardSolicitacao(props){
+  
+  const openModal = () => document.querySelector('#modal').classList.add('active');
+
+  const closeModal = () => document.querySelector('#modal').classList.remove('active');
+  
   return(
     <ContentCard>
             <CardProgressStyle>
@@ -9,11 +14,35 @@ function CardSolicitacao(props){
                 <p>{props.props.profissao}</p>
                 <ButtonProgress>
                     <AiFillCheckCircle size="40px" color="green"/>
-                    {/* <div id="pagar">Pagar</div> */}
-                    <button>Pagar</button>
+                    <button onClick={openModal}>Pagar</button>
                 </ButtonProgress>
                 <p id="description">{props.props.descricao}</p>
             </CardProgressStyle>
+            <div class="modal" id="modal">
+            <div class="modal__content">
+                <header class="modal__header">
+                    <h1>Efetuar pagamento</h1>
+                    <span id="close" onClick={closeModal}>&#10006;</span>
+                </header>
+                <div class="modal__main">
+                    <form class="form">
+                        <p>
+                          Mande uma foto do serviço realizado! (opcional)
+                          <input type="file"/>
+                        </p>
+                        
+                    </form>
+                </div>
+                <footer class="modal__footer">
+                    <button class="btn green" id="salvar">
+                      Pagar
+                    </button>
+                    <button class="btn blue" id="cancelar" onClick={closeModal}>
+                      Cancelar pagamento
+                    </button>
+                </footer>
+            </div>
+        </div>
         </ContentCard>
   );
 }
